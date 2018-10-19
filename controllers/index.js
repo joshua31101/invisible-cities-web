721b1ac43fbb47f21786ff232b3acace8a67eadd
Joshua Wang <joshuawang3110@gmail.com>
2018-10-19 10:12:05 -0400

    Add infinite scroll for statue list

--- controllers/index.js
+++ controllers/index.js
@@ -4,9 +4,39 @@ const firebase = require('./firebaseController');
 
 router.get('/', function(req, res) {
   const isAdminUser = req.session.isAdminUser;
+  const isScroll = req.query.isScroll;
+  let lastStatueKey = req.session.lastStatueKey;
+  // if the user visits the page and not scrolled
+  if (!isScroll) {
+    lastStatueKey = 0;
+  } else if (!lastStatueKey) {
+    return res.render('partials/statueCardContainer', {
+      statues: null,
+      isAdminUser,
+      maps: null,
+    });
+  }
 
-  firebase.getStatues(function(statues) {
+  firebase.getStatues(lastStatueKey, function(statues) {   
+    const statueKeyList = Object.keys(statues);   
+    const _lastStatueKey = statueKeyList[statueKeyList.length - 1];
+    // update last statue key for next pagination reference
+    req.session.lastStatueKey = _lastStatueKey;
+    // remove the last statue since it will be loaded in the next scroll
+    delete statues[_lastStatueKey];
+    // if the current statue list reaches last page, then set the next key null
+    // to go over from the beginning
+    if (statueKeyList.length < 17) {
+      req.session.lastStatueKey = null;
+    }
     firebase.getMaps(function(maps) {
+      if (isScroll) {
+        return res.render('partials/statueCardContainer', {
+          statues: statues,
+          isAdminUser,
+          maps: maps,
+        });
+      }
       res.render('statueList', {
         statues: statues,
         isAdminUser,
