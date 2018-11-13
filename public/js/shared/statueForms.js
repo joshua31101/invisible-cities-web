379f1a88750aac90c1a1d9f4a0463ec19c988da5
Joshua Wang <joshuawang3110@gmail.com>
2018-11-12 19:55:20 -0500

    Redesign filter in statue dashboard

--- public/js/shared/statueForms.js
+++ public/js/shared/statueForms.js
@@ -1,12 +1,32 @@
+// Flagging
+const FLAGGED_CLASSNAME = 'flagged';
+const UNFLAGGED_CLASSNAME = 'unflagged';
+const FLAG_BUTTON_TEXT = 'Flag for review';
+const UNFLAG_BUTTON_TEXT = 'Unflag';
+const FLAG_TAG_TEXT = 'Flagged';
+const FLAG_OPTION_CLASSNAME = 'flag-option';
+
+// Private/public
+const PRIVATE_CLASSNAME = 'private';
+const PUBLIC_CLASSNAME = 'public';
+const PUBLIC_BUTTON_TEXT = 'Set public';
+const PRIVATE_BUTTON_TEXT = 'Set private';
+const PRIVATE_TAG_TEXT = 'Private';
+const VISIBLE_OPTION_CLASSNAME = 'visible-option';
+
 (function() {
   $('.card-forms-container').on('submit', '.statue-form', function(ev) {
     ev.preventDefault();
 
     const $this = $(ev.target);
     const statueId = $this.find('input[name=statueId]').val();
+    const $cardContainer = $this.parents('.card-container');
+    let dataGroup = JSON.parse($cardContainer.attr('data-groups'));
+
     const isFlagForm = $this.hasClass('flag-form');
     const formData = { statueId: statueId };
     let $formInput;
+    
     if (isFlagForm) {
       $formInput = $this.find('input[name=isFlagged]');
       formData['isFlagged'] = $formInput.val();
@@ -24,36 +44,41 @@
         const isFlagged = data.isFlagged;
         const isPrivate = data.isPrivate;
         let btnClassnameBefore, btnClassnameAfter, tagText, btnText;
+
         if (!$tagContainer.length) {
           $tagContainer = $('<div class="tag-container"></div>');
           $statueCard.find('.card-body').prepend($tagContainer);
         }
 
         if (isFlagForm) {
-          btnClassname = 'flag-option';
-          tagText = 'Flagged';
+          btnClassname = FLAG_OPTION_CLASSNAME;
+          tagText = FLAG_TAG_TEXT;
           if (isFlagged) {
-            btnText = 'Unflag';
-            btnClassnameBefore = 'unflag';
-            btnClassnameAfter = 'flag';
+            btnText = UNFLAG_BUTTON_TEXT;
+            btnClassnameBefore = UNFLAGGED_CLASSNAME;
+            btnClassnameAfter = FLAGGED_CLASSNAME;
           } else {
-            btnText = 'Flag for review';
-            btnClassnameBefore = 'flag';
-            btnClassnameAfter = 'unflag';
+            btnText = FLAG_BUTTON_TEXT;
+            btnClassnameBefore = FLAGGED_CLASSNAME;
+            btnClassnameAfter = UNFLAGGED_CLASSNAME;
           }
         } else {
-          btnClassname = 'visible-option';
-          tagText = 'Private';
+          btnClassname = VISIBLE_OPTION_CLASSNAME;
+          tagText = PRIVATE_TAG_TEXT;
           if (isPrivate) {
-            btnText = 'Set public';
-            btnClassnameBefore = 'public';
-            btnClassnameAfter = 'private';
+            btnText = PUBLIC_BUTTON_TEXT;
+            btnClassnameBefore = PUBLIC_CLASSNAME;
+            btnClassnameAfter = PRIVATE_CLASSNAME;
           } else {
-            btnText = 'Set private';
-            btnClassnameBefore = 'private';
-            btnClassnameAfter = 'public';
+            btnText = PRIVATE_BUTTON_TEXT;
+            btnClassnameBefore = PRIVATE_CLASSNAME;
+            btnClassnameAfter = PUBLIC_CLASSNAME;
           }
         }
+        // toggle data group for Shuffle instances
+        dataGroup = dataGroup.filter(group => group !== btnClassnameBefore);
+        dataGroup.push(btnClassnameAfter);
+        $cardContainer.attr('data-groups', `["${dataGroup.join('","')}"]`);
         
         $formInput.val(true);
         if (isFlagged) {
